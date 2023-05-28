56c13e673d3dd4978db5618780b7380db32edc43
higton <matpaulo@hotmail.com.br>
2023-05-28 08:10:09 -0300

    feat: add sonar metrics
    Co-authored-by: Guilherme Braz <gvcerveirabraz@gmail.com>

--- src/sonarqube.ts
+++ src/sonarqube.ts
@@ -1,5 +1,21 @@
 export default class Sonarqube {
-  
-    constructor() {
-    }
+  private sonarMetrics = [
+    'files',
+    'functions',
+    'complexity',
+    'comment_lines_density',
+    'duplicated_lines_density',
+    'coverage',
+    'ncloc',
+    'tests',
+    'test_errors',
+    'test_failures',
+    'test_execution_time',
+    'security_rating',
+    'test_success_density',
+    'reliability_rating',
+  ]
+
+  constructor() {
+  }
 }
