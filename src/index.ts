fa4aad8420d8a5e158d9a3938022744e897e0a77
higton <matpaulo@hotmail.com.br>
2023-05-28 08:33:14 -0300

    feat: use Sonarqube class to get measures
    Co-authored-by: Guilherme Braz <gvcerveirabraz@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  const sonarQubeHost = core.getInput('host');
-  console.log(`sonarQubeHost: ${sonarQubeHost}`);
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error: any) {
-  core.setFailed(error.message);
+import Sonarqube from './sonarqube'
+
+async function run() {
+  try {
+    const { repo } = github.context
+    const sonarqube = new Sonarqube(repo)
+    const currentDate = new Date();
+
+    const measures = await sonarqube.getMeasures({
+      pageSize: 500,
+    })
+
+    console.log('Measures: ', measures);
+
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    const sonarQubeHost = core.getInput('host');
+    console.log(`sonarQubeHost: ${sonarQubeHost}`);
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
 }
+
+run();
