44e4300e2fed6075c00b074bef5c805b8b70043f
higton <matpaulo@hotmail.com.br>
2023-05-28 08:28:26 -0300

    feat: create constructor for Sonarqube class
    Co-authored-by: Guilherme Braz <gvcerveirabraz@gmail.com>

--- src/sonarqube.ts
+++ src/sonarqube.ts
@@ -1,21 +1,59 @@
+import axios, { AxiosInstance } from 'axios';
+import * as core from '@actions/core';
+
 export default class Sonarqube {
+  private http: AxiosInstance
+  public host: string
+  private token: string
+  public project: {
+    projectKey: string
+    projectBaseDir: string
+  }
   private sonarMetrics = [
-    'files',
-    'functions',
-    'complexity',
-    'comment_lines_density',
-    'duplicated_lines_density',
-    'coverage',
-    'ncloc',
-    'tests',
-    'test_errors',
-    'test_failures',
-    'test_execution_time',
-    'security_rating',
-    'test_success_density',
-    'reliability_rating',
+      'files',
+      'functions',
+      'complexity',
+      'comment_lines_density',
+      'duplicated_lines_density',
+      'coverage',
+      'ncloc',
+      'tests',
+      'test_errors',
+      'test_failures',
+      'test_execution_time',
+      'security_rating',
+      'test_success_density',
+      'reliability_rating',
   ]
 
-  constructor() {
+  constructor(repo: { owner: string; repo: string }) {
+      const info = this.getInfo(repo)
+
+      this.host = info.host
+      this.token = info.token
+      this.project = info.project
+      const tokenb64 = Buffer.from(`${this.token}:`).toString('base64')
+
+      console.log(`SonarQube host: ${this.host}`)
+      console.log(`SonarQube project: ${this.project.projectKey}`)
+
+      this.http = axios.create({
+          baseURL: this.host,
+          timeout: 10000,
+          headers: {
+            Authorization: this.token ? `Basic ${tokenb64}` : "",
+          }
+      })
   }
+
+  private getInfo = (repo: { owner: string; repo: string }) => ({
+    project: {
+        projectKey: core.getInput('projectKey')
+            ? core.getInput('projectKey')
+            : `${repo.owner}_${repo.repo}`,
+        projectBaseDir: core.getInput('projectBaseDir'),
+    },
+    host: core.getInput('host'),
+    token: core.getInput('token'),
+  })
 }
