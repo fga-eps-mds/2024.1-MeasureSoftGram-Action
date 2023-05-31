71bd99a14eb1317ee7c997376454f19a9535df67
higton <matpaulo@hotmail.com.br>
2023-05-31 10:46:16 -0300

    fix: change msgram version to 1.0.2
    Co-authored-by: Guilherme Braz <gvcerveirabraz@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ async function run() {
       console.log('Data written to file.');
     });
 
-    await exec('pip', ['install', 'msgram==1.1.0'])
+    await exec('pip', ['install', 'msgram==1.0.2'])
     await exec('msgram', ['init']);
     await exec('msgram', ['extract', '-o', 'sonarqube', '-dp', './analytics-raw-data/', '-ep', '.', '-le', 'py']);
     await exec('msgram', ['calculate', '-ep', '.', '-cp', '.msgram/', '-o', 'json']);
