10969bc4e438696410b26cbc8ec0aa4c3195ff73
Paulo <matpaulo@hotmail.com.br>
2023-06-18 10:01:29 -0300

    fix: adjust jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,19 @@
-module.exports = {
-  testMatch: ["**/*.test.ts"],
-  coverageDirectory: 'coverage',
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  bail: false,
+  clearMocks: true,
+  coverageProvider: "v8",
   preset: 'ts-jest',
+  setupFilesAfterEnv: ['./__tests__/jest.setup.ts'],
+  testEnvironment: "node",
+  testMatch: [
+    "**/__tests__/**/*.test.ts?(x)",
+  ],
+  coverageDirectory: 'coverage',
   reporters: [
     'default',
     [
